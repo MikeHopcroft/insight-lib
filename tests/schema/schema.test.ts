15f9d080038843fcb12c87aa5c33b709948a9c82
Dan Massey <danmass@microsoft.com>
2022-09-10 01:52:31 +0000

    more schemas and tests

--- tests/schema/schema.test.ts
+++ tests/schema/schema.test.ts
@@ -13,4 +13,35 @@ describe('validating valid json documents', () => {
     `;
     expect(validateJSONStr(accountStr)).toStrictEqual([true, '']);
   });
+
+  test('valid Capability is valid', () => {
+    const capabilityStr = `
+      {
+        "type": "Capability",
+        "id": "123456789ABCDEF0",
+        "name": "Tech check for patients",
+        "link": "https://is.visualstudio.com/IS/_workitems/edit/521396",
+        "description": "This is a valid Capability json object.",
+        "reported": "FY23 Q1",
+        "status": "Yellow",
+        "note": "This is a note with more context about the capability."
+      }
+    `;
+    expect(validateJSONStr(capabilityStr)).toStrictEqual([true, '']);
+  });
+
+  test('valid Customer Pattern is valid', () => {
+    const customerPatternStr = `
+      {
+        "type": "Customer Pattern",
+        "id": "23456789ABCDEF01",
+        "name": "Resilient Clusters",
+        "link": "https://is.visualstudio.com/IS/_workitems/edit/521397",
+        "description": "This is a valid Customer Pattern json object.",
+        "status": "Committed",
+        "indicator": "Yellow"
+      }
+    `;
+    expect(validateJSONStr(customerPatternStr)).toStrictEqual([true, '']);
+  });
 });
