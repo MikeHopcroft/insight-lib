169df7d8e5c9bc283cb77e86d5d010f8da7c01ca
Dan Massey <danmass@microsoft.com>
2022-09-11 10:50:51 +0000

    rewriting BizDate toward using a little parser

--- src/types/biz-date.ts
+++ src/types/biz-date.ts
@@ -1,135 +1,135 @@
+import {buildLexer} from 'typescript-parsec';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const regex =
   /^((FY|CY)(\d{4}|\d{2}))( (H(1|2)|Q(1|2|3|4)|(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)))?|TBD|Unknown$/;
 
-const months: {[key: string]: number} = {
-  Jan: 1,
-  Feb: 2,
-  Mar: 3,
-  Apr: 4,
-  May: 5,
-  Jun: 6,
-  Jul: 7,
-  Aug: 8,
-  Sep: 9,
-  Oct: 10,
-  Nov: 11,
-  Dec: 12,
-};
-
-export enum dateResolution {
-  Year,
-  Half,
-  Quarter,
-  Month,
-  Invalid,
+export enum YearKind {
+  CY,
+  FY,
+  TBD,
+  Unknown,
 }
+
+export enum YearPart {
+  H1,
+  H2,
+  Q1,
+  Q2,
+  Q3,
+  Q4,
+  Jan,
+  Feb,
+  Mar,
+  Apr,
+  May,
+  Jun,
+  Jul,
+  Aug,
+  Sep,
+  Oct,
+  Nov,
+  Dec,
+  None,
+}
+
+const Months = [
+  YearPart.None,
+  YearPart.Jan,
+  YearPart.Feb,
+  YearPart.Mar,
+  YearPart.Apr,
+  YearPart.May,
+  YearPart.Jun,
+  YearPart.Jul,
+  YearPart.Aug,
+  YearPart.Sep,
+  YearPart.Oct,
+  YearPart.Nov,
+  YearPart.Dec,
+];
 
 export class BizDate {
-  original: string;
-  fiscalNormalized: string;
+  year: YearKind;
+  part: YearPart;
   calendarYear: number;
   calendarMonth: number;
-  resolution: dateResolution;
-  valid: boolean;
 
-  constructor(str = '') {
-    let parts: [string, string, number, number, dateResolution] = [
-      '',
-      '',
-      0,
-      0,
-      dateResolution.Invalid,
-    ];
-    if (str === '') {
-      parts = thisMonth();
-      this.valid = true;
-    } else {
-      if (regex.test(str)) {
-        if (str === 'TBD' || str === 'Unknown') {
-          parts = [str, str, 9999, 12, dateResolution.Year];
-        } else {
-          const matches = regex.exec(str)!;
-          parts = parseMatches(str, matches[2], matches[3], matches[5]);
-        }
-        this.valid = true;
+  constructor(kind: YearKind, year: number, part: YearPart) {
+    this.year = kind;
+    if (kind === YearKind.CY || kind === YearKind.FY) {
+      this.part = part;
+      this.calendarMonth = calendarMonthFor(kind, part);
+      if (kind === YearKind.CY) {
+        this.calendarYear = year;
       } else {
-        parts = [str, '', 0, 0, dateResolution.Invalid];
-        this.valid = false;
+        this.calendarYear = fiscalToCalendarYear(year, this.calendarMonth);
       }
+    } else {
+      this.part = YearPart.None;
+      this.calendarYear = 9999;
+      this.calendarMonth = 12;
     }
-    this.original = parts[0];
-    this.fiscalNormalized = parts[1];
-    this.calendarYear = parts[2];
-    this.calendarMonth = parts[3];
-    this.resolution = parts[4];
+  }
+
+  toCalendarYear(): BizDate {
+    if (this.year === YearKind.FY) {
+      return new BizDate(
+        YearKind.CY,
+        this.calendarYear,
+        reverseYearPart(this.part)
+      );
+    } else {
+      return this;
+    }
+  }
+
+  toFiscalYear(): BizDate {
+    if (this.year === YearKind.FY) {
+      return new BizDate(
+        YearKind.FY,
+        calenderToFiscalYear(this.calendarYear, this.calendarMonth),
+        reverseYearPart(this.part)
+      );
+    } else {
+      return this;
+    }
+  }
+
+  toCYStr(): string {
+    return '';
+  }
+
+  toFYStr(): string {
+    return '';
   }
 }
 
-function thisMonth(): [string, string, number, number, dateResolution] {
-  const date = new Date();
-  const year = date.getUTCFullYear();
-  const month = date.getUTCMonth();
-  const calendar = `CY${year} ${toShortMonth(month)}`;
-  const fiscalYear = calenderToFiscalYear(year, month);
-  const fiscal = `FY${fiscalYear} ${toShortMonth(month)}`;
-  return [calendar, fiscal, year, month, dateResolution.Month];
+export function parseBizDate(str: string): BizDate {
+  return thisMonth();
 }
 
-function parseMatches(
-  str: string,
-  fcy: string,
-  y: string,
-  hqm: string | undefined
-): [string, string, number, number, dateResolution] {
-  const fiscal = fcy === 'FY';
-  let res = dateResolution.Year;
-  let month = 12;
-  let fiscal_hqm = '';
-  if (hqm !== undefined) {
-    switch (hqm) {
-      case 'H1':
-        month = fiscal ? 12 : 6;
-        fiscal_hqm = fiscal ? 'H1' : 'H2';
-        res = dateResolution.Half;
-        break;
-      case 'H2':
-        month = fiscal ? 6 : 12;
-        fiscal_hqm = fiscal ? 'H2' : 'H1';
-        res = dateResolution.Half;
-        break;
-      case 'Q1':
-        month = fiscal ? 9 : 3;
-        fiscal_hqm = fiscal ? 'Q1' : 'Q3';
-        res = dateResolution.Quarter;
-        break;
-      case 'Q2':
-        month = fiscal ? 12 : 6;
-        fiscal_hqm = fiscal ? 'Q2' : 'Q4';
-        res = dateResolution.Quarter;
-        break;
-      case 'Q3':
-        month = fiscal ? 3 : 9;
-        fiscal_hqm = fiscal ? 'Q3' : 'Q1';
-        res = dateResolution.Quarter;
-        break;
-      case 'Q4':
-        month = fiscal ? 6 : 12;
-        fiscal_hqm = fiscal ? 'Q4' : 'Q2';
-        res = dateResolution.Quarter;
-        break;
-      default:
-        month = toMonthOrd(hqm);
-        fiscal_hqm = hqm;
-        res = dateResolution.Month;
-    }
+export function thisMonth(yearKind = YearKind.CY): BizDate {
+  const dateNow = new Date();
+  const yearNow = dateNow.getUTCFullYear();
+  const monthNow = dateNow.getUTCMonth();
+  if (yearKind === YearKind.CY) {
+    return new BizDate(YearKind.CY, yearNow, yearPartFor(monthNow));
+  } else {
+    return new BizDate(
+      YearKind.FY,
+      calenderToFiscalYear(yearNow, monthNow),
+      yearPartFor(monthNow)
+    );
   }
-  let year = fiscal ? fiscalToCalendarYear(+y, month) : +y;
-  year = year < 2000 ? 2000 + year : year;
-  const fiscalStr =
-    res === dateResolution.Year
-      ? `FY${calenderToFiscalYear(year, month)}`
-      : `FY${calenderToFiscalYear(year, month)} ${fiscal_hqm}`;
-  return [str, fiscalStr, year, month, res];
+}
+
+export function thisQuarter(yearKind = YearKind.CY): BizDate {
+  return thisMonth(yearKind);
+}
+
+export function thisHalf(yearKind = YearKind.CY): BizDate {
+  return thisMonth(yearKind);
 }
 
 function calenderToFiscalYear(year: number, month: number): number {
@@ -148,15 +148,92 @@ function fiscalToCalendarYear(year: number, month: number): number {
   }
 }
 
-function toShortMonth(month: number): string {
-  for (const abbr in months) {
-    if (months[abbr] === month) {
-      return abbr;
-    }
+function calendarMonthFor(kind: YearKind, part: YearPart): number {
+  switch (part) {
+    case YearPart.H1:
+    case YearPart.Q2:
+      return kind === YearKind.CY ? 6 : 12;
+    case YearPart.H2:
+    case YearPart.Q4:
+      return kind === YearKind.CY ? 12 : 6;
+    case YearPart.Q1:
+      return kind === YearKind.CY ? 3 : 9;
+    case YearPart.Q3:
+      return kind === YearKind.CY ? 9 : 3;
+    case YearPart.Jan:
+      return 1;
+    case YearPart.Feb:
+      return 2;
+    case YearPart.Mar:
+      return 3;
+    case YearPart.Apr:
+      return 4;
+    case YearPart.May:
+      return 5;
+    case YearPart.Jun:
+      return 6;
+    case YearPart.Jul:
+      return 7;
+    case YearPart.Aug:
+      return 8;
+    case YearPart.Sep:
+      return 9;
+    case YearPart.Oct:
+      return 10;
+    case YearPart.Nov:
+      return 11;
+    case YearPart.Dec:
+      return 12;
+    default:
+      return 12;
   }
-  return '';
 }
 
-function toMonthOrd(month: string): number {
-  return months[month];
+function reverseYearPart(part: YearPart): number {
+  switch (part) {
+    case YearPart.H1:
+      return YearPart.H2;
+    case YearPart.H2:
+      return YearPart.H1;
+    case YearPart.Q1:
+      return YearPart.Q3;
+    case YearPart.Q2:
+      return YearPart.Q4;
+    case YearPart.Q3:
+      return YearPart.Q1;
+    case YearPart.Q4:
+      return YearPart.Q2;
+    default:
+      return part;
+  }
 }
+
+function yearPartFor(month: number): YearPart {
+  return Months[month];
+}
+
+enum TokenKind {
+  YearSpec,
+  YearNumber,
+  YearPart,
+  Month,
+  Number,
+  TBD,
+  Unknown,
+  Space,
+}
+
+const lexer = buildLexer([
+  [true, /^(FY)|(CY)/g, TokenKind.YearSpec],
+  [true, /^\d{4}|\d{2}/g, TokenKind.YearNumber],
+  [true, /^H|Q/g, TokenKind.YearPart],
+  [true, /^\d/g, TokenKind.Number],
+  [
+    true,
+    /^(Jan)|(Feb)|(Mar)|(Apr)|(May)|(Jun)|(Jul)|(Aug)|(Sep)|(Oct)|(Nov)|(Dec)/g,
+    TokenKind.Month,
+  ],
+  [true, /^TBD/g, TokenKind.TBD],
+  [true, /^Unknown/g, TokenKind.Unknown],
+  [false, /^\s/g, TokenKind.Space],
+]);
--- tests/types/biz-data.test.ts
+++ tests/types/biz-data.test.ts
@@ -1,68 +1,39 @@
-import {BizDate, dateResolution} from '../../src/types/biz-date';
+import {BizDate, YearKind, YearPart} from '../../src/types/biz-date';
 
 describe('constructing biz date', () => {
   test('with normal FY', () => {
-    expect(new BizDate('FY23 H1')).toStrictEqual(
-      Object.assign(new BizDate(), {
-        original: 'FY23 H1',
-        fiscalNormalized: 'FY2023 H1',
-        calendarYear: 2022,
-        calendarMonth: 12,
-        resolution: dateResolution.Half,
-        valid: true,
-      })
-    );
+    expect(new BizDate(YearKind.FY, 2023, YearPart.H1)).toMatchObject({
+      year: YearKind.FY,
+      part: YearPart.H1,
+      calendarYear: 2022,
+      calendarMonth: 12,
+    });
   });
 
   test('with CY', () => {
-    expect(new BizDate('CY2022 Q4')).toStrictEqual(
-      Object.assign(new BizDate(), {
-        original: 'CY2022 Q4',
-        fiscalNormalized: 'FY2023 Q2',
-        calendarYear: 2022,
-        calendarMonth: 12,
-        resolution: dateResolution.Quarter,
-        valid: true,
-      })
-    );
+    expect(new BizDate(YearKind.CY, 2022, YearPart.Q4)).toMatchObject({
+      year: YearKind.CY,
+      part: YearPart.Q4,
+      calendarYear: 2022,
+      calendarMonth: 12,
+    });
   });
 
   test('with month', () => {
-    expect(new BizDate('CY2022 Sep')).toStrictEqual(
-      Object.assign(new BizDate(), {
-        original: 'CY2022 Sep',
-        fiscalNormalized: 'FY2023 Sep',
-        calendarYear: 2022,
-        calendarMonth: 9,
-        resolution: dateResolution.Month,
-        valid: true,
-      })
-    );
+    expect(new BizDate(YearKind.CY, 2022, YearPart.Sep)).toMatchObject({
+      year: YearKind.CY,
+      part: YearPart.Sep,
+      calendarYear: 2022,
+      calendarMonth: 9,
+    });
   });
 
   test('with TBD', () => {
-    expect(new BizDate('TBD')).toStrictEqual(
-      Object.assign(new BizDate(), {
-        original: 'TBD',
-        fiscalNormalized: 'TBD',
-        calendarYear: 9999,
-        calendarMonth: 12,
-        resolution: dateResolution.Year,
-        valid: true,
-      })
-    );
-  });
-
-  test('with invalid date', () => {
-    expect(new BizDate('H1 23')).toStrictEqual(
-      Object.assign(new BizDate(), {
-        original: 'H1 23',
-        fiscalNormalized: '',
-        calendarYear: 0,
-        calendarMonth: 0,
-        resolution: dateResolution.Invalid,
-        valid: false,
-      })
-    );
+    expect(new BizDate(YearKind.TBD, 2022, YearPart.Sep)).toMatchObject({
+      year: YearKind.TBD,
+      part: YearPart.None,
+      calendarYear: 9999,
+      calendarMonth: 12,
+    });
   });
 });
