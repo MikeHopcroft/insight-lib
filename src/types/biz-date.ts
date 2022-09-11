f5371d557e5ce4a2c693a7af47941a7eb5190e98
Dan Massey <danmass@microsoft.com>
2022-09-11 16:35:37 +0000

    remove Mon -> number mapping

--- src/types/biz-date.ts
+++ src/types/biz-date.ts
@@ -256,37 +256,14 @@ function calendarMonthFor(kind: YearKind, part: YearPart): number {
     case YearPart.Year:
     case YearPart.H2:
     case YearPart.Q4:
+    case YearPart.None:
       return kind === YearKind.CY ? 12 : 6;
     case YearPart.Q1:
       return kind === YearKind.CY ? 3 : 9;
     case YearPart.Q3:
       return kind === YearKind.CY ? 9 : 3;
-    case YearPart.Jan:
-      return 1;
-    case YearPart.Feb:
-      return 2;
-    case YearPart.Mar:
-      return 3;
-    case YearPart.Apr:
-      return 4;
-    case YearPart.May:
-      return 5;
-    case YearPart.Jun:
-      return 6;
-    case YearPart.Jul:
-      return 7;
-    case YearPart.Aug:
-      return 8;
-    case YearPart.Sep:
-      return 9;
-    case YearPart.Oct:
-      return 10;
-    case YearPart.Nov:
-      return 11;
-    case YearPart.Dec:
-      return 12;
     default:
-      return 12;
+      return part - 6;
   }
 }
 
